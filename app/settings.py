3f948987d409ddfa694b802506cc7ac8bf692c2d
gessione_sena <fgssena@gmail.com>
2026-01-12 12:05:51 -0300

    database change

--- app/settings.py
+++ app/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+    'dev': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'sgvp',
         'USER': 'postgres',
@@ -96,7 +96,7 @@ DATABASES = {
         'HOST': 'sgvp_db',
         'PORT': '5432',
     },
-    'dev': {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
@@ -137,7 +137,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'app' / 'static',
