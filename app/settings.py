895f88fc30cdce97d77e8680c022327843475015
gessione_sena <fgssena@gmail.com>
2026-01-23 10:26:59 -0300

    Change the chart from pie to donut

--- app/settings.py
+++ app/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'app' / 'templates'], #['app/templates']
+        'DIRS': [BASE_DIR / 'app' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -89,14 +89,6 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'dev': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'sgvp',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'sgvp_db',
-        'PORT': '5432',
-    },
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
